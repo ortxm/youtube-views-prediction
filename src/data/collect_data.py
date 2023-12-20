b2310c4ee4e40552563d0bfa132414b946cdbfc1
justortem <archi6990@gmail.com>
2023-12-21 05:20:43 +0800

    the darkest hour: we have to pivot. pushing the project in its current state to preserve file structure as it was right now for future version control.

--- src/data/collect_data.py
+++ src/data/collect_data.py
@@ -1,123 +1,48 @@
-# collect_data.py
-
+import os
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-import json
-import pandas as pd
+from googleapiclient.errors import HttpError  # Import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2.credentials import Credentials
 
-# Set your API key (replace 'YOUR_API_KEY' with your actual API key)
-API_KEY = 'AIzaSyDOiOOGjskuedf2x-q-f9SLZm24w0_wbwo'
+SCOPES = ['https://www.googleapis.com/auth/yt-analytics.readonly']
 
-# Set the YouTube API service
-youtube = build('youtube', 'v3', developerKey=API_KEY)
-
-flow = (InstalledAppFlow.from_client_secrets_file(
-    'client_secrets.json', scopes=['https://www.googleapis.com/auth/yt-analytics.readonly']))
-credentials = Credentials.from_authorized_user_file('token.json')
-youtube_analytics = build('youtubeAnalytics', 'v2', credentials=credentials)
+API_SERVICE_NAME = 'youtubeAnalytics'
+API_VERSION = 'v2'
+CLIENT_SECRETS_FILE = 'client_secrets.json'
 
 
-def get_video_data(video_id, youtube_service):
-    """
-    Get data for a specific video.
-    """
+def get_service():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_local_server(port=0)
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+
+def execute_api_request(client_library_function, **kwargs):
     try:
-        response = youtube_service.videos().list(
-            part='snippet,statistics,contentDetails',
-            id=video_id
+        response = client_library_function(
+            **kwargs
         ).execute()
 
-        # Extract relevant data
-        statistics_data = response['items'][0]['statistics']
-        content_details_data = response['items'][0]['contentDetails']
-
-        # Extract numeric features
-        views = int(statistics_data.get('viewCount', 0))
-        likes = int(statistics_data.get('likeCount', 0))
-        dislikes = int(statistics_data.get('dislikeCount', 0))
-        comments = int(statistics_data.get('commentCount', 0))
-
-        # Convert video duration to seconds
-        duration = pd.to_timedelta(
-            content_details_data.get('duration')).total_seconds() if 'duration' in content_details_data else 0
-
-        return {
-            'views': views,
-            'likes': likes,
-            'dislikes': dislikes,
-            'comments': comments,
-            'duration': duration
-        }
+        print(response)
 
     except HttpError as e:
-        print(f'Error: {e}')
+        print(f'HttpError: {e}')
 
 
-def get_video_analytics(video_id, start_date, end_date, youtube_analytics_service):
-    """
-    Get historical view data for a specific video using YouTube Analytics API.
-    """
-    try:
-        # Make API request to YouTube Analytics API
-        analytics_response = youtube_analytics_service.reports().query(
-            ids=f'channel==@NotAlbino',
-            startDate=start_date,
-            endDate=end_date,
-            metrics='views',
-            dimensions='video',
-            filters=f'video=={video_id}',
-            includeHistoricalChannelData=True
-        ).execute()
+if __name__ == '__main__':
+    # Disable OAuthlib's HTTPs verification when running locally.
+    # *DO NOT* leave this option enabled when running in production.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-        # Extract and return relevant data
-        analytics_data = analytics_response.get('rows', [])
-        return analytics_data
-
-    except HttpError as e:
-        print(f'Error: {e}')
-        return None
-
-
-def authenticate_youtube():
-    """
-    Authenticate YouTube API using OAuth2.
-    """
-    try:
-        local_flow = InstalledAppFlow.from_client_secrets_file(
-            'client_secrets.json', scopes=['https://www.googleapis.com/auth/youtube.readonly'])
-        local_credentials = local_flow.run_local_server(port=0)
-        return local_credentials
-    except Exception as e:
-        print(f'Authentication Error: {e}')
-        return None
-
-
-def main():
-    video_id = 'cyeaGI2UyDs'
-    start_date = '2023-01-01'
-    end_date = '2023-12-31'
-
-    # Authenticate YouTube API
-    cred = authenticate_youtube()
-    if not cred:
-        return
-
-    # Set the YouTube API service
-    youtube_service = build('youtube', 'v3', credentials=credentials)
-
-    # Set the YouTube Analytics API service
-    youtube_analytics_service = build('youtubeAnalytics', 'v2', credentials=credentials)
-
-    # Continue with the rest of the script...
-    video_data = get_video_data(video_id, youtube_service)
-    video_analytics = get_video_analytics(video_id, start_date, end_date, youtube_analytics_service)
-
-    # Print or save the collected data
-    print("Video Data:", json.dumps(video_data, indent=2))
-    print("Video Analytics:", json.dumps(video_analytics, indent=2))
-
-
-if __name__ == "__main__":
-    main()
+    youtubeAnalytics = get_service()
+    execute_api_request(
+        youtubeAnalytics.reports().query,
+        ids='channel==@NotAlbino',
+        startDate='2017-01-01',
+        endDate='2017-12-31',
+        metrics='estimatedMinutesWatched,views,likes,subscribersGained',
+        dimensions='day',
+        sort='day'
+    )
+girt
