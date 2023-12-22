3f4896f78b65456ed8e89c6994891648b4e6264d
justortem <archi6990@gmail.com>
2023-12-22 18:08:17 +0800

    committing current version for backup's sake; we have not yet decided the direction of the pivot

--- src/data/collect_data.py
+++ src/data/collect_data.py
@@ -1,48 +1,117 @@
 import os
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
+import json
+import pandas as pd
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError  # Import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
 
-SCOPES = ['https://www.googleapis.com/auth/yt-analytics.readonly']
+# Set your YouTube Data API key (replace 'YOUR_API_KEY' with your actual API key)
+API_KEY = 'AIzaSyDOiOOGjskuedf2x-q-f9SLZm24w0_wbwo'
 
-API_SERVICE_NAME = 'youtubeAnalytics'
-API_VERSION = 'v2'
-CLIENT_SECRETS_FILE = 'client_secrets.json'
+# Set the YouTube API service
+youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 
-def get_service():
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_local_server(port=0)
-    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
-
-
-def execute_api_request(client_library_function, **kwargs):
+def get_video_data(video_id):
+    """
+    Get data for a specific video using the YouTube Data API.
+    """
     try:
-        response = client_library_function(
-            **kwargs
+        response = youtube.videos().list(
+            part='snippet,statistics',
+            id=video_id
         ).execute()
 
-        print(response)
+        # Extract relevant data
+        snippet_data = response['items'][0]['snippet']
+        statistics_data = response['items'][0]['statistics']
 
-    except HttpError as e:
-        print(f'HttpError: {e}')
+        # Extract desired features
+        title = snippet_data.get('title', '')
+        description = snippet_data.get('description', '')
+        views = int(statistics_data.get('viewCount', 0))
+        likes = int(statistics_data.get('likeCount', 0))
+        dislikes = int(statistics_data.get('dislikeCount', 0))
+        comments = int(statistics_data.get('commentCount', 0))
+
+        return {
+            'title': title,
+            'description': description,
+            'views': views,
+            'likes': likes,
+            'dislikes': dislikes,
+            'comments': comments
+        }
+
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
 
 
-if __name__ == '__main__':
-    # Disable OAuthlib's HTTPs verification when running locally.
-    # *DO NOT* leave this option enabled when running in production.
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+def save_to_json(data, filename):
+    """
+    Save data to a JSON file.
+    """
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=2)
 
-    youtubeAnalytics = get_service()
-    execute_api_request(
-        youtubeAnalytics.reports().query,
-        ids='channel==@NotAlbino',
-        startDate='2017-01-01',
-        endDate='2017-12-31',
-        metrics='estimatedMinutesWatched,views,likes,subscribersGained',
-        dimensions='day',
-        sort='day'
-    )
-girt
+
+def save_to_csv(data, filename):
+    """
+    Save data to a CSV file.
+    """
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
+
+def collect_data(_channel_id, _max_results):
+    """
+    Collect video data for a channel using the YouTube Data API.
+    """
+    print("Before YouTube Data API Request\n")
+    try:
+        print("Before YouTube Data API Request but inside of try\n")
+        # Make API request to get video IDs from the channel
+        search_response = youtube.search().list(
+            part='id',
+            channelId=_channel_id,
+            type='video',
+            maxResults=_max_results
+        ).execute()
+
+        print("After YouTube Data API Request")
+
+        video_ids = [item['id']['videoId'] for item in search_response['items']]
+
+        # Collect detailed data for each video
+        videos_data = []
+        for video_id in video_ids:
+            video_data = get_video_data(video_id)
+            if video_data:
+                videos_data.append(video_data)
+
+        return videos_data
+
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
+
+
+if __name__ == "__main__":
+    # Set channel ID and maximum number of results to collect
+    channel_id = '@NotAlbino'
+    max_results = 317
+
+    # Collect video data
+    collected_data = collect_data(channel_id, max_results)
+
+    if collected_data:
+        # Save raw data to JSON
+        raw_filename = os.path.join('datasets', 'raw', 'raw_data.json')
+        save_to_json(collected_data, raw_filename)
+        print(f'Raw data saved to {raw_filename}')
+
+        # Save processed data to CSV
+        processed_filename = os.path.join('datasets', 'processed', 'processed_data.csv')
+        save_to_csv(collected_data, processed_filename)
+        print(f'Processed data saved to {processed_filename}')
+    else:
+        print('Data collection failed.')
