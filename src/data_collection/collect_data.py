90086618b4ea4a3126e02179c38de9268351947b
justortem <archi6990@gmail.com>
2023-12-28 07:06:03 +0800

    minor tree adjustments

--- src/data_collection/collect_data.py
+++ src/data_collection/collect_data.py
@@ -1,117 +1,137 @@
+# collect_data.py
+
 import os
 import json
-import pandas as pd
+import csv
+from datetime import datetime, timedelta
 from googleapiclient.discovery import build
 
-# Set your YouTube Data API key (replace 'YOUR_API_KEY' with your actual API key)
+# Set your YouTube API key
 API_KEY = 'AIzaSyDOiOOGjskuedf2x-q-f9SLZm24w0_wbwo'
 
-# Set the YouTube API service
-youtube = build('youtube', 'v3', developerKey=API_KEY)
+# Define the channels to collect data from
+channels = [
+    {'name': 'Channel1', 'id': 'ChannelID1'},
+    {'name': 'Channel2', 'id': 'ChannelID2'},
+]
+
+# Specify the number of videos to retrieve from each channel
+videos_per_channel = 30
+
+# Specify the directory where you want to save the CSV file
+dataset_directory = 'datasets'
+
+# Specify the file name
+csv_filename = 'video_data.csv'
+
+# Construct the full file path
+csv_filepath = os.path.abspath(os.path.join(dataset_directory, csv_filename))
 
 
-def get_video_data(video_id):
+def is_same_day(timestamp1, timestamp2):
     """
-    Get data for a specific video using the YouTube Data API.
+    Check if two timestamps are from the same day.
     """
-    try:
-        response = youtube.videos().list(
-            part='snippet,statistics',
+    date1 = datetime.utcfromtimestamp(timestamp1).date()
+    date2 = datetime.utcfromtimestamp(timestamp2).date()
+    return date1 == date2
+
+
+def get_video_data(api_key, channel_id, max_results):
+    """
+    Get data for the latest videos from a specific channel.
+    """
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    # Make API request to get the latest videos from the channel
+    response = youtube.search().list(
+        part='id',
+        channelId=channel_id,
+        type='video',
+        order='date',
+        maxResults=max_results
+    ).execute()
+
+    video_data = []
+
+    for item in response.get('items', []):
+        video_id = item['id']['videoId']
+        video_info = youtube.videos().list(
+            part='snippet,statistics,contentDetails',
             id=video_id
         ).execute()
 
         # Extract relevant data
-        snippet_data = response['items'][0]['snippet']
-        statistics_data = response['items'][0]['statistics']
+        snippet = video_info['items'][0]['snippet']
+        statistics = video_info['items'][0]['statistics']
+        content_details = video_info['items'][0]['contentDetails']
 
-        # Extract desired features
-        title = snippet_data.get('title', '')
-        description = snippet_data.get('description', '')
-        views = int(statistics_data.get('viewCount', 0))
-        likes = int(statistics_data.get('likeCount', 0))
-        dislikes = int(statistics_data.get('dislikeCount', 0))
-        comments = int(statistics_data.get('commentCount', 0))
+        video_data.append({
+            'duration': content_details['duration'],
+            'upload_date': snippet['publishedAt'],
+            'views': statistics.get('viewCount', 0),
+            'likes': statistics.get('likeCount', 0),
+            'dislikes': statistics.get('dislikeCount', 0),
+            'comments': statistics.get('commentCount', 0)
+        })
 
-        return {
-            'title': title,
-            'description': description,
-            'views': views,
-            'likes': likes,
-            'dislikes': dislikes,
-            'comments': comments
-        }
-
-    except Exception as e:
-        print(f'Error: {e}')
-        return None
+    return video_data
 
 
-def save_to_json(data, filename):
+def write_to_csv(file_path, data):
     """
-    Save data to a JSON file.
+    Write video data to a CSV file.
     """
-    with open(filename, 'w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, ensure_ascii=False, indent=2)
+    with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['duration', 'upload_date', 'views', 'likes', 'dislikes', 'comments']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
 
 
-def save_to_csv(data, filename):
+def get_top_channels(api_key, max_results):
     """
-    Save data to a CSV file.
+    Get a list of top channels based on view count.
     """
-    df = pd.DataFrame(data)
-    df.to_csv(filename, index=False)
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    # Make API request to get channels based on search query
+    response = youtube.search().list(
+        part='snippet',
+        type='channel',
+        maxResults=max_results,
+        order='viewCount',  # Sorting by view count
+        q='popular channels'  # You can adjust the search query as needed
+    ).execute()
+
+    top_channels = [{'name': item['snippet']['title'], 'id': item['id']['channelId']} for item in response.get('items', [])]
+
+    return top_channels
 
 
-def collect_data(_channel_id, _max_results):
-    """
-    Collect video data for a channel using the YouTube Data API.
-    """
-    print("Before YouTube Data API Request\n")
-    try:
-        print("Before YouTube Data API Request but inside of try\n")
-        # Make API request to get video IDs from the channel
-        search_response = youtube.search().list(
-            part='id',
-            channelId=_channel_id,
-            type='video',
-            maxResults=_max_results
-        ).execute()
+def main():
+    # Check if data was already collected today
+    today = datetime.utcnow().date()
+    if os.path.exists(csv_filepath) and is_same_day(today, datetime.utcfromtimestamp(os.path.getctime(csv_filepath)).date()):
+        print("Data already collected today. Exiting.")
+        return
 
-        print("After YouTube Data API Request")
+    top_channels = get_top_channels(API_KEY, 10)  # Adjust the number as needed
 
-        video_ids = [item['id']['videoId'] for item in search_response['items']]
+    channels = top_channels
+    # Collect video data from each channel
+    all_video_data = []
+    for channel in channels:
+        channel_name = channel['name']
+        channel_id = channel['id']
+        print(f"Collecting data for {channel_name}...")
+        video_data = get_video_data(API_KEY, channel_id, videos_per_channel)
+        all_video_data.extend(video_data)
 
-        # Collect detailed data for each video
-        videos_data = []
-        for video_id in video_ids:
-            video_data = get_video_data(video_id)
-            if video_data:
-                videos_data.append(video_data)
-
-        return videos_data
-
-    except Exception as e:
-        print(f'Error: {e}')
-        return None
+    # Write data to CSV file
+    write_to_csv(csv_filepath, all_video_data)
+    print("Data collection completed.")
 
 
 if __name__ == "__main__":
-    # Set channel ID and maximum number of results to collect
-    channel_id = '@NotAlbino'
-    max_results = 317
-
-    # Collect video data
-    collected_data = collect_data(channel_id, max_results)
-
-    if collected_data:
-        # Save raw data to JSON
-        raw_filename = os.path.join('datasets', 'raw', 'raw_data.json')
-        save_to_json(collected_data, raw_filename)
-        print(f'Raw data saved to {raw_filename}')
-
-        # Save processed data to CSV
-        processed_filename = os.path.join('datasets', 'processed', 'processed_data.csv')
-        save_to_csv(collected_data, processed_filename)
-        print(f'Processed data saved to {processed_filename}')
-    else:
-        print('Data collection failed.')
+    main()
